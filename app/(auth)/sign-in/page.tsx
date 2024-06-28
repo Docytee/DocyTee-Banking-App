c10a0625efee90bbc41664c8d3e8d9bb09a37e95
Docytee <titilopenasir@gmail.com>
2024-06-28 11:11:50 +0100

    Docytee Banking App

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,21 @@
+// import AuthForm from '@/components/AuthForm'
+
+// const SignIn = () => {
+//   return (
+//     <section className="flex-center size-full max-sm:px-6">
+//       <AuthForm type="sign-in" />
+//     </section>
+//   )
+// }
+
+// export default SignIn
+
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <div>SignIn</div>
+  )
+}
+
+export default SignIn
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,20 @@
+// import AuthForm from '@/components/AuthForm'
+
+// const SignUp = async () => {
+//   return (
+//     <section className="flex-center size-full max-sm:px-6">
+//       <AuthForm type="sign-up" />
+//     </section>
+//   )
+// }
+
+// export default SignUp
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div>SignUp</div>
+  )
+}
+
+export default SignUp
